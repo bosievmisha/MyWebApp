3d0e7dc55585a9f16dcde287879f26b3703c429a
Босиев Михаил <misha.bosiev@mail.ru>
2024-07-24 20:58:36 +0300

    Добавьте файлы проекта.

---
+++ RunGroup/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using RunGroup.Models;
+using Microsoft.EntityFrameworkCore;
+namespace RunGroup.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ RunGroup/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroup.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Women,
+        City,
+        Trail,
+        Endurance
+    }
+}
